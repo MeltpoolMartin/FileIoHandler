9419361b691d507536961af1d3e0f0398baa2f6d
MeltpoolMartin <martinwachter@gmx.net>
2020-06-14 21:44:16 +0200

    This commit enables clang-format for this repository. Also added a convenient shell script for easy execution and applied clang-format to all src files

--- include/FileIoHandler.hpp
+++ include/FileIoHandler.hpp
@@ -1,14 +1,13 @@
-#include <fstream>
 #include <filesystem>
+#include <fstream>
 #include <optional>
 
 class FileIoHandler {
-    private:
-        std::filesystem::path       m_FilePath;
-    
-    public:
-                                    FileIoHandler(std::string& filePath);
-        std::optional<std::string>  read();
-        void                        write(std::string& data);
+ private:
+  std::filesystem::path m_FilePath;
 
+ public:
+  FileIoHandler(std::string &filePath);
+  std::optional<std::string> read();
+  void write(std::string &data);
 };
--- src/FileIoHandler.cpp
+++ src/FileIoHandler.cpp
@@ -1,50 +1,44 @@
+#include "FileIoHandler.hpp"
+
 #include <sstream>
 #include <stdexcept>
-#include "FileIoHandler.hpp"
 
 namespace fs = std::filesystem;
 
-FileIoHandler::FileIoHandler(std::string& filePath) :
-m_FilePath(filePath)
-{
-    m_FilePath = filePath;
+FileIoHandler::FileIoHandler(std::string &filePath) : m_FilePath(filePath) {
+  m_FilePath = filePath;
 }
 
 std::optional<std::string> FileIoHandler::read() {
-    if(fs::exists(m_FilePath)) {
-        std::ifstream stream;
-        stream.open(m_FilePath, std::ios::in);
-        if(!stream.is_open()) {
-            return {};
-        }
-        else {
-            std::string data;
-            std::ostringstream ss;
-            ss << stream.rdbuf();
-            data = ss.str();
-            stream.close();
-            if(data.empty()) {
-                return {};
-            }
-            else {
-                return data;
-            }
-
-        }
+  if (fs::exists(m_FilePath)) {
+    std::ifstream stream;
+    stream.open(m_FilePath, std::ios::in);
+    if (!stream.is_open()) {
+      return {};
+    } else {
+      std::string data;
+      std::ostringstream ss;
+      ss << stream.rdbuf();
+      data = ss.str();
+      stream.close();
+      if (data.empty()) {
+        return {};
+      } else {
+        return data;
+      }
     }
-    else {
-        throw std::invalid_argument("File does not exit");
-    }
+  } else {
+    throw std::invalid_argument("File does not exit");
+  }
 }
 
-void FileIoHandler::write(std::string& data) {
-    std::ofstream stream;
-    stream.open(m_FilePath, std::ios::out);
-    if(stream.is_open()) {
-        stream << data;
-        stream.close();
-    }
-    else {
-        throw std::invalid_argument("Invalid file path");
-    }
+void FileIoHandler::write(std::string &data) {
+  std::ofstream stream;
+  stream.open(m_FilePath, std::ios::out);
+  if (stream.is_open()) {
+    stream << data;
+    stream.close();
+  } else {
+    throw std::invalid_argument("Invalid file path");
+  }
 }
